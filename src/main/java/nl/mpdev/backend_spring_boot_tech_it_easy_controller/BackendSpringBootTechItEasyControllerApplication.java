5b72c6ae3ffb2ea68b428a04033c8d968d3beb69
Maarten Postma <maarten.postma1000@gmail.com>
2024-09-04 10:07:32 +0200

    Initial commit

---
+++ src/main/java/nl/mpdev/backend_spring_boot_tech_it_easy_controller/BackendSpringBootTechItEasyControllerApplication.java
@@ -0,0 +1,13 @@
+package nl.mpdev.backend_spring_boot_tech_it_easy_controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendSpringBootTechItEasyControllerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendSpringBootTechItEasyControllerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nl/mpdev/backend_spring_boot_tech_it_easy_controller/BackendSpringBootTechItEasyControllerApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.mpdev.backend_spring_boot_tech_it_easy_controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendSpringBootTechItEasyControllerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
